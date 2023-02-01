b90d45c6b13209e9dbd43feb132f0269b77c87cd
Sam Peddamatham <peddamat@gmail.com>
2023-02-01 00:50:14 +0000

    Refactor.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,67 @@ use std::io::SeekFrom;
 use openssl::rsa::Rsa;
 use openssl::bn::BigNum;
 
+///////////////////////////////////////////////////////////////////////////////
+// Root Cert Store
+///////////////////////////////////////////////////////////////////////////////
+#[derive(BinRead)]
+#[br(magic = b"\x11\xF1\x12\xF2\x13\xF3\x14\xF4\x15\xF5\x16\xF6\x17\xF7\x18\xF8")]
+struct RootCertStore {
+    count: u32,
+
+    #[br(count = count)]
+    certs: Vec<RSCert>,
+}
+
+#[derive(BinRead)]
+#[allow(dead_code)]
+struct RSCert {
+    name_hash: [u8; 20],
+    start: RSTime,
+    end: RSTime,
+    data: RSCertData,
+}
+
+#[binrw::binread]
+#[allow(dead_code)]
+enum RSCertData {
+    #[br(little, magic = 1u32)]
+    Rsa {
+        #[br(temp)] n_sz: u16,
+        #[br(temp)] e_sz: u16,
+
+        #[br(align_after = 4, count = n_sz, map = |s: Vec<u8>| BigNum::from_slice(&s).unwrap())]
+        n: BigNum,
+        #[br(align_after = 4, count = e_sz, map = |s: Vec<u8>| BigNum::from_slice(&s).unwrap())]
+        e: BigNum,
+    },
+    #[br(little, magic = 2u32)]
+    Ecdsa {
+        curve_id: u16,
+
+        #[br(temp)] key_sz: u16,
+
+        #[br(count = key_sz)]
+        d: Vec<u8>,
+    },
+}
+
+#[derive(BinRead)]
+#[allow(dead_code)]
+struct RSTime {
+    year: u16,
+    month: u8,
+    day: u8,
+    hour: u8,
+    minute: u8,
+    #[br(pad_after = 1)]
+    second: u8,
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// TLS Cert Store
+///////////////////////////////////////////////////////////////////////////////
 #[binrw::binread]
 #[br(magic = b"\xab\xfe\x18\x5b\x70\xc3\x46\x92")]
 struct TlsStore {
@@ -66,60 +127,6 @@ struct RSAPrivKey {
     qinv: BigNum,
 }
 
-#[derive(BinRead)]
-#[br(magic = b"\x11\xF1\x12\xF2\x13\xF3\x14\xF4\x15\xF5\x16\xF6\x17\xF7\x18\xF8")]
-struct RootCertStore {
-    count: u32,
-
-    #[br(count = count)]
-    certs: Vec<RSCert>,
-}
-
-#[derive(BinRead)]
-#[allow(dead_code)]
-struct RSCert {
-    name_hash: [u8; 20],
-    start: RSTime,
-    end: RSTime,
-    data: RSCertData,
-}
-
-#[binrw::binread]
-#[allow(dead_code)]
-enum RSCertData {
-    #[br(little, magic = 1u32)]
-    Rsa {
-        #[br(temp)] n_sz: u16,
-        #[br(temp)] e_sz: u16,
-
-        #[br(align_after = 4, count = n_sz, map = |s: Vec<u8>| BigNum::from_slice(&s).unwrap())]
-        n: BigNum,
-        #[br(align_after = 4, count = e_sz, map = |s: Vec<u8>| BigNum::from_slice(&s).unwrap())]
-        e: BigNum,
-    },
-    #[br(little, magic = 2u32)]
-    Ecdsa {
-        curve_id: u16,
-
-        #[br(temp)] key_sz: u16,
-
-        #[br(count = key_sz)]
-        d: Vec<u8>,
-    },
-}
-
-#[derive(BinRead)]
-#[allow(dead_code)]
-struct RSTime {
-    year: u16,
-    month: u8,
-    day: u8,
-    hour: u8,
-    minute: u8,
-    #[br(pad_after = 1)]
-    second: u8,
-}
-
 fn main() -> io::Result<()> {
     let file_path = "firmware/atwinc1500-original.bin";
 
@@ -134,6 +141,27 @@ fn main() -> io::Result<()> {
         let reader = Cursor::new(buf);
         reader
     };
+
+    // Locate and parse Root Cert Store
+    reader.seek(SeekFrom::Start(0x4000)).expect("Error finding Root Cert Store!");
+    {
+        let rs: RootCertStore = reader.read_le().unwrap();
+
+        println!("Found {} Root Store certs!", rs.count);
+
+        for (i, cert) in rs.certs.into_iter().enumerate() {
+            match cert.data {
+                RSCertData::Rsa { n, e } => {
+                    let pk = openssl::rsa::Rsa::from_public_components(n, e).unwrap();
+                    let pkk = pk.public_key_to_pem_pkcs1().unwrap();
+                    println!( "Root Certificate (RSA) {}:\n {}", i, String::from_utf8(pkk).unwrap());
+                },
+                RSCertData::Ecdsa { curve_id: _, d: _, } => {
+                    println!("Root Certificate (ECDSA) {}:\n Name: {:?}!", i, cert.name_hash);
+                }
+            }
+        }
+    }
 
     // Locate and parse TLS Store
     reader.seek(SeekFrom::Start(0x5000)).expect("Error finding TLS Cert Store!");
@@ -164,27 +192,6 @@ fn main() -> io::Result<()> {
         }
     }
 
-
-    // Locate and parse Root Cert Store
-    reader.seek(SeekFrom::Start(0x4000)).expect("Error finding Root Cert Store!");
-    {
-        let rs: RootCertStore = reader.read_le().unwrap();
-
-        println!("Found {} Root Store certs!", rs.count);
-
-        for (i, cert) in rs.certs.into_iter().enumerate() {
-            match cert.data {
-                RSCertData::Rsa { n, e } => {
-                    let pk = openssl::rsa::Rsa::from_public_components(n, e).unwrap();
-                    let pkk = pk.public_key_to_pem_pkcs1().unwrap();
-                    println!( "Root Certificate (RSA) {}:\n {}", i, String::from_utf8(pkk).unwrap());
-                },
-                RSCertData::Ecdsa { curve_id: _, d: _, } => {
-                    println!("Root Certificate (ECDSA) {}:\n Name: {:?}!", i, cert.name_hash);
-                }
-            }
-        }
-    }
 
 
     Ok(())
